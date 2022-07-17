c1ebbbe6e1249b162860037a2a44ae22fff5ab59
VladimirKrutko <krutkovova24@gmail.com>
2022-07-17 20:35:13 +0200

    add_post_model

--- innotter/post/models.py
+++ innotter/post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Class with page model
+    """
+    page = models.ForeignKey('page.Page',to_field='uuid',on_delete=models.DO_NOTHING, related_name='posts')
+    content = models.CharField(max_length=180)
+    reply_to = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, related_name='replies')
+    like = models.ManyToManyField('user.User', related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
