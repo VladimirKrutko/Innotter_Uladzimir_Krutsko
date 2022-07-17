9acb068460320910ec1d047daba48af7da6190ab
VladimirKrutko <krutkovova24@gmail.com>
2022-07-17 20:19:56 +0200

    add_page_model

--- innotter/page/models.py
+++ innotter/page/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+class Page(models.Model):
+    """
+    Class with Page model
+    """
+    name = models.CharField(max_length=80)
+    uuid = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='tag')
+    owner = models.ForeignKey('user.User',to_field='User_id', on_delete=models.CASCADE, related_name='owner')
+    followers = models.ManyToManyField('user.User',related_name='follows')
+    image = models.URLField(null=True, blank=True)
+    is_private = models.BooleanField(default=False)
+    follow_requests = models.ManyToManyField('user.User', related_name='requests')
+    unblock_date = models.DateTimeField(null=True, blank=True)
+
