110c5e8b537873cc96745c1effe8a55863db6c57
VladimirKrutko <krutkovova24@gmail.com>
2022-07-17 19:27:11 +0200

    create project and apps

--- innotter/innotter/asgi.py
+++ innotter/innotter/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for inov_tw project.
+ASGI config for innotter project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inov_tw.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innotter.settings')
 
 application = get_asgi_application()
