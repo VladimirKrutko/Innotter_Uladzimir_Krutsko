5b70d3caa40b2125c991d1edf9ece712a7af0d79
VladimirKrutko <krutkovova24@gmail.com>
2022-07-20 11:14:11 +0200

    change_docker

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /usr/src/app
 ADD innotter  ./
 COPY requarements.txt ./
+COPY entrypoint.sh ./
 
 RUN pip install -r requarements.txt
-
-
+RUN chmod +x entrypoint.sh
+# ENTRYPOINT "entrypoint.sh"
 
---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/src/app bash
+python innotter/manage.py makemigrations user
+python innotter/manage.py makemigrations page
+python innotter/manage.py migrate
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python innotter/manage.py createsuperuser \
+        --noinput \
+        --title $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_USERNAME
+fi
+python innotter/manage.py runserver 0.0.0.0:8000
+$@
