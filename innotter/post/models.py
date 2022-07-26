b1ce259a4e2a037d43d2911ebd0e9e62f77c418c
VladimirKrutko <krutkovova24@gmail.com>
2022-07-26 11:49:30 +0200

    Create_post_model

--- innotter/post/models.py
+++ innotter/post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    page_id = models.ForeignKey('page.Page', on_delete=models.PROTECT)
+    reply_to = models.ForeignKey('post.Post', on_delete=models.PROTECT)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField('user.User')
+
+    def __str__(self):
+        return str(self.id) + '_' + str(self.page_id)
+
+
+
+
+
+
---
+++ innotter/post/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from rest_framework.serializers import ValidationError
+from rest_framework.permissions import IsAuthenticated
+
+
+class PostSerialize(serializers.ModelSerializer):
+    pass
