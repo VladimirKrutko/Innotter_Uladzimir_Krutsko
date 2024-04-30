fcb2266e1ce287b9e37c9cef1d5abd292411a153
Uladzimir Krutsko <52661053+VladimirKrutko@users.noreply.github.com>
2024-04-30 12:35:23 +0200

    Update settings.py

--- innotter/innotter/settings.py
+++ innotter/innotter/settings.py
@@ -74,26 +74,26 @@ WSGI_APPLICATION = 'innotter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'pgdb',
-#         'PORT': 5432,
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'innotter_db',
-        'USER': 'innotter',
-        'PASSWORD': '1458',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'innotter_db',
+#         'USER': 'innotter',
+#         'PASSWORD': '1458',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
